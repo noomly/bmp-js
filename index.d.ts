a9bc506752eba5cad6ae41b9b2b363228211405f
noom <echojnow@student.42.fr>
2019-10-16 15:09:27 +0200

    Delete some properties

--- index.d.ts
+++ index.d.ts
@@ -1,18 +1,6 @@
 export interface BmpData {
-    fileSize: number,
-    reserved: number,
-    offset: number,
-    headerSize: number,
     width: number,
     height: number,
-    planes: number,
-    bitPP: number,
-    compress: number,
-    rawSize: number,
-    hr: number,
-    vr: number,
-    colors: number,
-    importantColors: number,
     data: Buffer,
 }
 
